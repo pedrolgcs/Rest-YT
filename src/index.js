98d4456e88dac1dabf0c284468cac771a9a9a805
Pedro Henrique <pedro.lg.cs@gmail.com>
2018-08-05 19:23:01 -0300

    add async and await

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@
 const server = require('./server')
 // passo a porta
 server.listen('3000')
+
+console.log(server.address())
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,10 @@
 // importo o restify
 const restify = require('restify')
 // crio o servidor
-const server = restify.createServer()
+const server = restify.createServer({
+  name: 'curso_restify',
+  version: '1.0.0'
+})
 // import o metodo de rotas
 const routes = require('../http/routes')
 // importo o cors
